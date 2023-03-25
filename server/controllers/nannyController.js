aa0c76b03f59e6fe0fb28786b722d0afa724eb70
omar hussain <omarhussain23moh@gmail.com>
2023-03-25 13:52:32 -0300

    get nanny API implementation

--- server/controllers/nannyController.js
+++ server/controllers/nannyController.js
@@ -4,10 +4,10 @@ import Nanny from "../models/nanny.model.js";
 export async function getNannies(req, res) {
     try {
         const nannies = await Nanny.find({});
-        if (!nannies) return res.status(501).send({ error: "Couldn't Find the Nannies." });
-        console.log(nannies);
-        res.status(200).send(nannies.json());
+        if (!nannies) return res.status(501).send({ error: "Couldn't Find Nannies." });
+        res.status(200).send({ data: nannies });
     } catch (e) {
         return res.status(404).send({ error: "Cannot Find Nanny Data." });
     }
 }
+
