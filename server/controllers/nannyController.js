565d4fe8c02adcc77c89f1ce89e72cb3f584934a
omar hussain <omarhussain23moh@gmail.com>
2023-03-27 11:02:00 -0300

    filtering nanny based on pricerange and speciality

--- server/controllers/nannyController.js
+++ server/controllers/nannyController.js
@@ -25,15 +25,18 @@ export async function getNanniesBySpecialization(req, res) {
     }
 }
 
-/** POST: http://localhost:3002/api/nanny/getNannies */
-// export async function createNanny(req, res) {
-//     try {
-//         const nannies = await Nanny.find({});
-//         if (!nannies) return res.status(501).send({ error: "Couldn't Find Nannies." });
-//         console.log(nannies);
-//         res.status(200).send(nannies.json());
-//     } catch (e) {
-//         return res.status(404).send({ error: "Cannot Find Nanny Data." });
-//     }
-// }
+/** GET: http://localhost:3002/api/nanny/getNanniesByPriceRange/30 */
+export async function getNanniesByPriceRange(req, res) {
+    console.log("Pricing was called!");
+    try {
+        const { priceRange } = req.params;
+        const nannies = await Nanny.find({ "price": { $lte: priceRange } });
+        if (!nannies) return res.status(501).send({ error: `Couldn't Find Nannies with ${speciality} specialization.` });
+        console.log(nannies);
+        res.status(200).send({ data: nannies });
+    } catch (e) {
+        return res.status(404).send({ error: "Data not found." });
+    }
+}
+
 
--- server/routes/nannyRoutes.js
+++ server/routes/nannyRoutes.js
@@ -6,5 +6,6 @@ const nannyRoutes = Router();
 /** GET Methods */
 nannyRoutes.route('/getNannies').get(authDecodeToken, nannyController.getNannies); // user with username
 nannyRoutes.route('/getNanniesBySpecialization/:speciality').get(authDecodeToken, nannyController.getNanniesBySpecialization);
+nannyRoutes.route('/getNanniesByPriceRange/:priceRange').get(nannyController.getNanniesByPriceRange);
 
 export default nannyRoutes;
