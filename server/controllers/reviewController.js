8e46538e1ad29fb348165d00eb864018c15e764e
omar hussain <omarhussain23moh@gmail.com>
2023-04-09 17:16:02 -0300

    delete and post review routes implmentation

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -1,19 +1,13 @@
 import Nanny from "../models/nanny.model.js";
 import Review from "../models/review.model.js";
-import User from "../models/user.Model.js";
+import User from "../models/user.model.js";
 
-/** POST: http://localhost:3002/api/nanny/reviews/:nannyId
-*/
+/** POST: http://localhost:3002/api/reviews/:userId/:nannyId */
 export async function createReviews(req, res) {
-    console.log("Create Review was called!!");
     try {
-        let { nannyId } = req.params;
-        console.log(req.body);
-        console.log(req.params);
-        const user = await User.findById();
-        console.log(user);
+        let { userId, nannyId } = req.params;
+        const user = await User.findById(userId);
         const nannies = await Nanny.findById(nannyId);
-        console.log(nannies);
         if (!nannies) return res.status(501).send({ error: "Couldn't Find Nannies to create reviews." });
         const review = new Review(req.body.review);
         review.author = user._id;
@@ -25,3 +19,24 @@ export async function createReviews(req, res) {
         return res.status(404).send({ error: "Something went wrong creating reviews." });
     }
 }
+
+/** DELETE: http://localhost:3002/api/reviews/:userId/:nannyId/:reviewId */
+export async function deleteReview(req, res) {
+    try {
+        const { nannyId, reviewId } = req.params;
+        console.log(req.params);
+        try {
+            await Nanny.findByIdAndUpdate(nannyId, { $pull: { reviews: [reviewId] } });
+        } catch (e) {
+            return res.status(501).send({ error: "Couldn't Find Nannies to delete reviews." });
+        }
+        try {
+            await Review.findByIdAndDelete(reviewId);
+        } catch (e) {
+            return res.status(501).send({ error: "Unable to delete find and delete reviews." });
+        }
+        res.status(200).send("Review deleted successfully!");
+    } catch (e) {
+        return res.status(404).send({ error: "Something went wrong deleting reviews." });
+    }
+};
--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { authDecodeToken } from "../middleware/middleware.js";
+import { authDecodeToken, isReviewAuthor } from "../middleware/middleware.js";
 import * as reviewController from "../controllers/reviewController.js";
 const reviewRoutes = Router();
 
 /** POST Methods */
-reviewRoutes.route('/:nannyId').post(reviewController.createReviews);
+reviewRoutes.route('/:userId/:nannyId').post(authDecodeToken, reviewController.createReviews);
 
 /** DELETE Methods */
-// reviewRoutes.route('/:reviewId').delete(authDecodeToken, reviewController.deleteReview);
+reviewRoutes.route('/:userId/:nannyId/:reviewId').delete(authDecodeToken, isReviewAuthor, reviewController.deleteReview);
 
 export default reviewRoutes;
