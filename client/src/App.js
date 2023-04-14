595c2d6c902739e51e315a5f5e7a1e96033c0f71
Carly Cormier <w0467979@campus.nscc.ca>
2023-04-14 14:41:04 -0300

    create header footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { createRoutes } from './routes/routes';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
 
   return (
+    <>
+    <Header />
     <main>
       <RouterProvider router={createRoutes()}></RouterProvider>
     </main>
+    <Footer />
+    </>
   )
 }
 
